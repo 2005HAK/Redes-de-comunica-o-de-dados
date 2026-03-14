be3fa8bb608a06ba4b4aa57926afb33f2165269d
2005HAK <hebertkubis15@gmail.com>
2026-03-14 20:25:27 -0300

    correção multimedia

--- Exercicio_Busca_ativa/client/compiler.sh
+++ Exercicio_Busca_ativa/client/compiler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install  qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qtcreator libqt5widgets5 build-essential
+sudo apt install  qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qtcreator libqt5widgets5 qtmultimedia5-dev libqt5multimedia5-plugins build-essential
 
 mkdir -p build
 rm -rf build/*
