f967dfa453215426600e2baa356b2c866033d509
2005HAK <hebertkubis15@gmail.com>
2026-03-14 11:06:00 -0300

    Criação do device controler, device e bd. Inicio da criação da interface

--- Exercicio - Busca ativa/model/device.hpp
+++ Exercicio - Busca ativa/model/device.hpp
@@ -19,10 +19,13 @@ class Device {
 		std::string getName() const { return this->name; }
 		std::string getIp() const { return this->ip; }
 		std::string getMac() const { return this->mac; }
+		uint16_t getInterval() const { return this->captureInterval; }
 
 		bool isActive() const { return this->active; }
 
 		void setInterval(uint16_t min) { this->captureInterval = min; }
+
+		bool setActive(bool active) { this->active = active; }
 };
 
 #endif // DEVICE_HPP
---
+++ Exercicio - Busca ativa/view/interface.hpp
@@ -0,0 +1,29 @@
+#ifndef INTERFACE_HPP
+#define INTERFACE_HPP
+
+#include <QMainWindow>
+#include <QTabWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QListWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QSpinBox>
+#include "../controller/devicecontroller.hpp"
+
+class Interface : public QMainWindow {
+	private:
+		DeviceController* controller;
+
+		void handleRegister();
+		void handleDeviceSelection(QListWidgetItem *item);
+		void handleInstantCapture();
+		void handleToggleStatus();
+
+	public:
+		Interface(DeviceController& controller, QWidget *parent = nullptr);
+
+		~Interface();
+};
+
+#endif // INTERFACE_HPP
