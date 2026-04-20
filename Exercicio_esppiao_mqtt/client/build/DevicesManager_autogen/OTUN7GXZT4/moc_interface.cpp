cd6efc99fe6d8b2580a90ab1ae613220012bbed9
2005HAK <hebertkubis15@gmail.com>
2026-04-19 21:33:13 -0300

    teste 1 mqtt

---
+++ Exercicio_esppiao_mqtt/client/build/DevicesManager_autogen/OTUN7GXZT4/moc_interface.cpp
@@ -0,0 +1,143 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'interface.hpp'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.15)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../view/interface.hpp"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'interface.hpp' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.15. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Interface_t {
+    QByteArrayData data[10];
+    char stringdata0[147];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Interface_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Interface_t qt_meta_stringdata_Interface = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "Interface"
+QT_MOC_LITERAL(1, 10, 14), // "handleRegister"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 21), // "handleDeviceSelection"
+QT_MOC_LITERAL(4, 48, 16), // "QListWidgetItem*"
+QT_MOC_LITERAL(5, 65, 4), // "item"
+QT_MOC_LITERAL(6, 70, 18), // "handleDeleteDevice"
+QT_MOC_LITERAL(7, 89, 20), // "handleInstantCapture"
+QT_MOC_LITERAL(8, 110, 18), // "handleToggleStatus"
+QT_MOC_LITERAL(9, 129, 17) // "handleSetInterval"
+
+    },
+    "Interface\0handleRegister\0\0"
+    "handleDeviceSelection\0QListWidgetItem*\0"
+    "item\0handleDeleteDevice\0handleInstantCapture\0"
+    "handleToggleStatus\0handleSetInterval"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Interface[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   44,    2, 0x08 /* Private */,
+       3,    1,   45,    2, 0x08 /* Private */,
+       6,    0,   48,    2, 0x08 /* Private */,
+       7,    0,   49,    2, 0x08 /* Private */,
+       8,    0,   50,    2, 0x08 /* Private */,
+       9,    0,   51,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Interface::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Interface *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->handleRegister(); break;
+        case 1: _t->handleDeviceSelection((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 2: _t->handleDeleteDevice(); break;
+        case 3: _t->handleInstantCapture(); break;
+        case 4: _t->handleToggleStatus(); break;
+        case 5: _t->handleSetInterval(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Interface::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_Interface.data,
+    qt_meta_data_Interface,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Interface::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Interface::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Interface.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int Interface::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 6;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
